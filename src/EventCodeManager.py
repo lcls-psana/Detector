bcefcd815287e4189f59ef9270823dd95e958bae
msdubrovin <msdubrovin@gmail.com>
2021-03-18 14:36:30 -0700

    add methods event_codes, event_code_list

--- src/EventCodeManager.py
+++ src/EventCodeManager.py
@@ -7,8 +7,8 @@ Usage::
     from Detector.EventCodeManager import EventCodeManager
     evcode = '1,2,4'
     ecm = EventCodeManager(evcode, verbos=0)
-    for evt ds.events() :
-        if not ecm.select(evt) : continue 
+    for evt ds.events():
+        if not ecm.select(evt): continue 
 
 Methods:
   * :py:meth:`EventCodeManager.set_event_codes`
@@ -19,12 +19,12 @@ If you use all or part of it, please give an appropriate acknowledgment.
 
 Created (moved from app/det_ndarr_raw_proc) on 2017-10-20 by Mikhail Dubrovin
 """
-from __future__ import print_function
-#------------------------------
+#from __future__ import print_function
+#---
 
 from Detector.EvrDetector import EvrDetector
 
-class EventCodeManager(object) :
+class EventCodeManager(object):
     """Class-helper for event selection using event code.
 
     The string of comma-separated event codes is passed in evcode through option -c, (ex.: -c 1,2,4).
@@ -32,25 +32,25 @@ class EventCodeManager(object) :
     If any of input event codes is set negative - then all event codes are used to deselect event.  
     More complicated logical formulaes are not supprorted.
     """
-    def __init__(self, evcode=None, verbos=0) :
+    def __init__(self, evcode=None, verbos=0):
         self.verbos = verbos
         self.set_event_codes(evcode)
         self.evrdet = EvrDetector(':Evr')
         self.do_check = self.lst_ec is not None and self.evrdet is not None
-        if self.verbos & 1 : print('%s: list of requested event codes: %s, do_check=%s' %\
+        if self.verbos & 1: print('%s: list of requested event codes: %s, do_check=%s' %\
            (self.__class__.__name__, str(self.lst_ec), self.do_check))
         self.counter = 0
 
 
-    def set_event_codes(self, evcode) :
+    def set_event_codes(self, evcode):
         """Splits input (str) for list of integer event codes. Any negative event code inverts selection decision for all event codes."""
 
-        if evcode is None or evcode.lower() == 'none' :
+        if evcode is None or evcode.lower() == 'none':
             self.lst_ec = None
             return
-        try :
+        try:
             lst_ec = [int(field) for field in evcode.split(',')] # convert string to a list of int event codes
-        except :
+        except:
             self.lst_ec = None
             return
             
@@ -58,18 +58,28 @@ class EventCodeManager(object) :
         self.lst_ec = [abs(ec) for ec in lst_ec]             # list of unsigned integer event codes
 
 
-    def select(self, evt) :
+    def event_codes(self, evt):
+        """Returns list of event codes for evt."""
+        return self.evrdet.eventCodes(evt)
+
+
+    def event_code_list(self):
+        """Returns list of event codes to select."""
+        return self.lst_ec
+
+
+    def select(self, evt):
         """Returns True/False to select/deselect event."""
-        if not self.do_check : return True
+        if not self.do_check: return True
         evt_codes = self.evrdet.eventCodes(evt)
         ec_in_event = any(ec in evt_codes for ec in self.lst_ec)
         res = (not ec_in_event) if self.do_deselect else ec_in_event
-        if self.counter < 20 :
+        if self.counter < 20:
             self.counter += 1
-            if self.verbos & 2 : 
+            if self.verbos & 2:
                 print('%s.select: evt_codes=%s, result=%s' % (self.__class__.__name__, str(evt_codes), res))
-                if self.counter == 20 : print('%s.select: stop printing messages' % (self.__class__.__name__))
+                if self.counter == 20: print('%s.select: stop printing messages' % (self.__class__.__name__))
         return res
 
-#------------------------------
+# EOF
 
