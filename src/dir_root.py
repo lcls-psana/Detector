fc2a9e1219b8a5884552a83daca914669d510c56
msdubrovin <msdubrovin@gmail.com>
2022-09-06 09:38:19 -0700

    add common root directory for calibration repositories and logging

---
+++ src/dir_root.py
@@ -0,0 +1,12 @@
+"""define DIR_ROOT for repositories and logfiles through the environment variable:
+   /reg/g/psdm # lcls
+   /cds/group/psdm # lcls2
+   /sdf/g/psdm  # on s3df etc.
+"""
+import os
+
+DIR_ROOT = os.getenv('SIT_ROOT')
+DIR_REPO = os.path.join(DIR_ROOT, 'detector/calib/constants/')
+DIR_LOG_AT_START = os.path.join(DIR_ROOT, 'logs/atstart/')
+
+# EOF
