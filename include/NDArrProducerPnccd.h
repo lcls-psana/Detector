5f78d1f5fb42e97d25a99a4db4aab71a57fd57a9
msdubrovin <msdubrovin@gmail.com>
2022-09-14 10:15:53 -0700

    get rid of compilation warnings, cosmetic - remove blanks

--- include/NDArrProducerPnccd.h
+++ include/NDArrProducerPnccd.h
@@ -1,18 +1,13 @@
 #ifndef DETECTOR_NDARRPRODUCERPNCCD_H
 #define DETECTOR_NDARRPRODUCERPNCCD_H
 
-//--------------------------------------------------------------------------
 // File and Version Information:
 // 	$Id$
 //
 // Description:
 //	Class NDArrProducerPnccd
 //
-//------------------------------------------------------------------------
 
-//----------------------
-// Base Class Headers --
-//----------------------
 #include "Detector/NDArrProducerBase.h"
 
 //-----------------
@@ -23,13 +18,7 @@
 //#include <sstream>   // for stringstream
 //#include <boost/shared_ptr.hpp>
 
-//-------------------------------
-// Collaborating Class Headers --
-//-------------------------------
-//#include "Detector/GlobalMethods.h"
 #include "psddl_psana/pnccd.ddl.h"
-
-//-----------------------------
 
 namespace Detector {
 
@@ -40,7 +29,7 @@ namespace Detector {
  *
  *  @brief Produces ndarray<TOUT,3> from pnCCD camera raw data.
  *
- *  @note This software was developed for the LCLS project.  If you use all or 
+ *  @note This software was developed for the LCLS project.  If you use all or
  *  part of it, please give an appropriate acknowledgment.
  *
  *  @version $Id$
@@ -50,19 +39,17 @@ namespace Detector {
  *  @see NDArrProducerBase, NDArrProducerStore
  */
 
-//-----------------------------
-
 class NDArrProducerPnccd : public NDArrProducerBase {
 public:
 
-  /// Data type for detector image 
+  /// Data type for detector image
   typedef uint16_t data_t;
 
-  const static size_t   Segs   = 4; 
-  //const static size_t   Rows   = 512; 
-  //const static size_t   Cols   = 512; 
-  //const static size_t   FrSize = Rows*Cols; 
-  //const static size_t   Size   = Segs*Rows*Cols; 
+  const static size_t   Segs   = 4;
+  const static size_t   Rows   = 512;
+  const static size_t   Cols   = 512;
+  //const static size_t   FrSize = Rows*Cols;
+  //const static size_t   Size   = Segs*Rows*Cols;
 
   // Constructor
   NDArrProducerPnccd(const PSEvt::Source& source, const unsigned& mode=0, const unsigned& pbits=0, const float& vdef=0);
@@ -92,15 +79,13 @@ private:
 
   void print_warning(const char* msg="");
 
-//-----------------------------
-
   /**
    * @brief Get pnccd four frames from Psana::PNCCD::FramesV1 data object and copy them in the ndarray<TOUT, 3> out_ndarr
    * Returns false if data is missing.
    */
 
   template <typename TFRAME, typename TOUT>
-  ndarray<const data_t, 3> 
+  ndarray<const data_t, 3>
   getNDArrForType(PSEvt::Event& evt, PSEnv::Env& env) {
 
       m_count_ent ++;
@@ -108,28 +93,29 @@ private:
       boost::shared_ptr<TFRAME> frames1 = evt.get(m_source, m_key, &m_src);
       if (frames1) {
 
-	  const unsigned* shape_data = (frames1->frame(0)).data().shape();
-        
-          ndarray<TOUT, 3> nda_out = make_ndarray<TOUT>(Segs, shape_data[0], shape_data[1]);
-          typename ndarray<TOUT, 3>::iterator it_out = nda_out.begin(); 
+	  // const unsigned* shape_data = (frames1->frame(0)).data().shape();
+          // shape_data[0] -  may be used uninitialized in this function
+          // ndarray<TOUT, 3> nda_out = make_ndarray<TOUT>(Segs, shape_data[0], shape_data[1]);
+          ndarray<TOUT, 3> nda_out = make_ndarray<TOUT>(Segs, Rows, Cols);
+          typename ndarray<TOUT, 3>::iterator it_out = nda_out.begin();
 
-          std::stringstream str; 
+          std::stringstream str;
 
           if(m_pbits & 2) str << "  numLinks = " << frames1->numLinks();
 
           for (unsigned i = 0 ; i != frames1->numLinks(); ++ i) {
-          
-              const Psana::PNCCD::FrameV1& frame = frames1->frame(i);          
+
+              const Psana::PNCCD::FrameV1& frame = frames1->frame(i);
               const ndarray<const data_t, 2> data = frame.data();
 
-              if(m_pbits & 2) {      
-                str << "\n  Frame #" << i;          
+              if(m_pbits & 2) {
+                str << "\n  Frame #" << i;
                 str << "\n    specialWord = " << frame.specialWord();
                 str << "\n    frameNumber = " << frame.frameNumber();
                 str << "\n    timeStampHi = " << frame.timeStampHi();
-                str << "\n    timeStampLo = " << frame.timeStampLo();          
+                str << "\n    timeStampLo = " << frame.timeStampLo();
                 str << "\n    frame size  = " << data.shape()[0] << 'x' << data.shape()[1];
-              }      
+              }
 
               // Copy frame from data to output ndarray with changing type
               for ( ndarray<const data_t, 2>::iterator it=data.begin(); it!=data.end(); ++it, ++it_out) {
@@ -140,16 +126,13 @@ private:
           if(m_pbits & 2) { str << "\n    nda_out:\n" << nda_out; MsgLog(name(), info, str.str()); }
 
           return nda_out;
-      }
+      }  // if (frames1)
 
       ndarray<TOUT,3> nda;
       return nda;
   }
 
-//-----------------------------
-//-----------------------------
-
-}; // class 
+}; // class
 
 } // namespace Detector
 
