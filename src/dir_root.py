0b40b7c9d09e70746b3576f059643e4bd310a0a5
msdubrovin <msdubrovin@gmail.com>
2022-10-14 16:48:36 -0700

    add DIR_PSDM_DATA = os.getenv('SIT_PSDM_DATA')

--- src/dir_root.py
+++ src/dir_root.py
@@ -1,14 +1,14 @@
 """define DIR_ROOT for repositories and logfiles through the environment variable:
    /reg/g/psdm # lcls
    /cds/group/psdm # lcls2
-   /sdf/g/psdm  # on s3df etc.
+   /sdf/group/lcls/ds/ana/  # on s3df etc.
 """
 import os
 
-DIR_ROOT = os.getenv('SIT_ROOT')  # /reg/g/psdm
+DIR_ROOT = os.getenv('SIT_ROOT')
 DIR_REPO = os.path.join(DIR_ROOT, 'detector/calib/constants/')
-DIR_LOG_AT_START = os.path.join(DIR_ROOT, 'logs/atstart/')
+DIR_LOG_AT_START = os.path.join(DIR_ROOT, 'detector/logs/atstart/')
 
-DIR_ROOT_DATA = '/reg/d/psdm'  # dcs
-
+DIR_PSDM_DATA = os.getenv('SIT_PSDM_DATA')
+DIR_ROOT_DATA = DIR_PSDM_DATA # '/reg/d/psdm'  # dcs
 # EOF
