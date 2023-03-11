ac9b1a50290757bceef25533eb8ac7cae1cf288e
msdubrovin <msdubrovin@gmail.com>
2023-03-10 16:16:07 -0800

    add DIR_REPO_STATUS

--- src/dir_root.py
+++ src/dir_root.py
@@ -8,6 +8,7 @@ import os
 DIR_ROOT = os.getenv('SIT_ROOT')
 DIR_REPO = os.path.join(DIR_ROOT, 'detector/calib/constants/')
 DIR_LOG_AT_START = os.path.join(DIR_ROOT, 'detector/logs/atstart/')
+DIR_REPO_STATUS = os.path.join(DIR_ROOT, 'detector/calib/constants/status/')
 
 DIR_PSDM_DATA = os.getenv('SIT_PSDM_DATA')
 DIR_ROOT_DATA = DIR_PSDM_DATA # '/reg/d/psdm'  # dcs
