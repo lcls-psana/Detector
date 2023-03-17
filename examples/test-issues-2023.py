de01f3459933f1862d90ff7bdfce02b648ddd561
msdubrovin <msdubrovin@gmail.com>
2023-03-17 09:32:19 -0700

    add code to create/test status_data

--- examples/test-issues-2023.py
+++ examples/test-issues-2023.py
@@ -10,16 +10,21 @@ import sys
 SCRNAME = sys.argv[0].rsplit('/')[-1]
 
 
-def test_pixel_status_extra():
+def test_status_extra(shape=(704, 768)):
     import numpy as np
-    fname = '/cds/data/psdm/XPP/xpptut15/calib/Epix100a::CalibV1/XcsEndstation.0:Epix100a.1/pixel_status_extra/0-end.data'
-    a = np.zeros((704, 768), dtype=np.uint16)
-    a[100:600, 100:600] = 1
-    a[150:550, 150:550] = 0
-    a[200:500, 200:500] = 2
-    a[250:450, 250:450] = 0
-    a[300:400, 300:400] = 4
-    a[330:370, 330:370] = 0
+    fname = '/cds/data/psdm/XPP/xpptut15/calib/Epix100a::CalibV1/XcsEndstation.0:Epix100a.1/status_extra/0-end.data'
+    a = np.zeros(shape, dtype=np.uint16)
+    for i in range(12): a[50*i:50*i+20, :] = i
+    np.savetxt(fname, a, fmt='%d')
+    print('saved test calibration constants:\n  %s' % fname)
+    return a
+
+
+def test_status_data(shape=(704, 768)):
+    import numpy as np
+    fname = '/cds/data/psdm/XPP/xpptut15/calib/Epix100a::CalibV1/XcsEndstation.0:Epix100a.1/status_data/0-end.data'
+    a = np.zeros(shape, dtype=np.uint16)
+    for i in range(12): a[:, 50*i:50*i+20] = i
     np.savetxt(fname, a, fmt='%d')
     print('saved test calibration constants:\n  %s' % fname)
     return a
@@ -39,20 +44,26 @@ def issue_2023_03_14():
     det = psana.Detector('XcsEndstation.0:Epix100a.1')
     print('calibDir:', ds.env().calibDir())
 
-    status_extra_deploy = test_pixel_status_extra()
+    #Deploy test constants to the calib directory
+    arr1 = test_status_extra()
+    arr2 = test_status_data()
+
+    print('test access')
     status = det.status(runnum)
     status_extra = det.status_extra(runnum)
-    mask = det.status_as_mask(runnum, mstcode=0xffff, mstextra=0x3)
-
-    print(gu.info_ndarr(status, 'status:', last=10))
+    status_data = det.status_data(runnum)
+    print(gu.info_ndarr(status,       'pixel_status:', last=10))
     print(gu.info_ndarr(status_extra, 'status_extra:', last=10))
+    print(gu.info_ndarr(status_data,  'status_data: ', last=10))
 
-    #img = status_extra
-    gg.plotImageLarge(status_extra, amp_range=(-1, 4), title='status_extra')
-    gg.plotImageLarge(mask, amp_range=(-1, 4), title='mask')
+    mask = det.status_as_mask(runnum, mstcode=0xffff, mstextra=0xffff, mstdata=0xffff)
+
+    gg.plotImageLarge(status, amp_range=(-1, 4), title='pixel_status')
+    gg.plotImageLarge(status_extra, amp_range=(-1, 12), title='status_extra')
+    gg.plotImageLarge(status_data, amp_range=(-1, 12), title='status_data')
+    gg.plotImageLarge(mask, amp_range=(-1, 4), title='status_as_mask')
     gg.show()
 
-#======================
 
 def issue_2023_MM_DD():
     """ISSUE:
