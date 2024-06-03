96c67bacadb48a063360b789de3cc95ade4f233f
msdubrovin <msdubrovin@gmail.com>
2024-06-03 09:15:12 -0700

    add test for junngfrau calib with non-default parameters for Silke

--- test/pytest_detector.py
+++ test/pytest_detector.py
@@ -1,11 +1,11 @@
 """
    Usage::
-   pytest Detector/test/pytest_detector
+   pytest Detector/test/pytest_detector.py
 """
 import sys
 import psana
 from Detector.GlobalUtils import np, info_ndarr
-from data_test_access.absolute_path import os, path_to_xtc_test_file
+from data_test_access.absolute_path import os, path_to_xtc_test_file, path_to_calibdir
 from PSCalib.GlobalUtils import create_directory
 
 def _stdout(s):
@@ -17,6 +17,13 @@ def path_exists(path, msg='\n  file exists: '):
     assert os.path.exists(path)
     print('%s%s' % (msg, path))
 
+def data_exists(path='/tmp/data_test'):
+    """restore data in path if not available"""
+    if os.path.exists(path): return
+    cmd = 'git clone git@github.com:lcls-psana/data_test %s' % path
+    print('execute command: %s' % cmd)
+    os.system(cmd)
+
 def logname_prefix(dirwork, prefix='log-subproc-det'):
     create_directory(dirwork, mode=0o775, group='ps-users')
     return os.path.join(dirwork, prefix)
@@ -27,6 +34,14 @@ def compare_det_raw(fname_xtc, detname='XppGon.0:Epix100a.1', expected='shape:(7
     evt = next(ds.events())
     s = info_ndarr(det.raw(evt))
     print('\n  1-st event raw: %s' % s.rstrip('\n'))
+    assert s == expected
+
+def compare_det_calib(fname_xtc, detname='XppGon.0:Epix100a.1', expected='shape:(704, 768) size:540672 dtype:uint16 [3861 4050 3976 3992 3778...]', **kwa):
+    ds = psana.DataSource(fname_xtc)
+    det = psana.Detector(detname)
+    evt = next(ds.events())
+    s = info_ndarr(det.calib(evt))
+    print('\n  1-st event calib: %s' % s.rstrip('\n'))
     assert s == expected
 
 def is_turned_off(turned_off=True):
@@ -93,7 +108,8 @@ class det_epix100a():
         print('__init__ for %s' % self.__class__.__name__)
         self.fname_xtc = path_to_xtc_test_file(fname='data-xppn4116-r0137-3events-epix100a.xtc')
         #print('\n\n======== %s' % self.__class__.__name__) ##.split('_')[-1])
-        self.dircalib = './calib'
+        #self.dircalib = './calib'
+        self.dircalib = path_to_calibdir()
         self.dirwork = './work'
         self.logname = logname_prefix(self.dirwork, prefix='log-subproc-epix100a')
         psana.setOption('psana.calib-dir', self.dircalib)
@@ -103,7 +119,7 @@ class det_epix100a():
     def __del__(self):
         print('__del__ for %s' % self.__class__.__name__)
 
-#    def fnish(self):
+#    def finish(self):
 #        """
 #        """
 #        if self.test_det_completed:
@@ -111,6 +127,7 @@ class det_epix100a():
 
     def xtc_file_is_available(self):
         """- test that xtc file is available for epix100a"""
+        data_exists()
         path_exists(self.fname_xtc)
 
     def det_raw(self):
@@ -147,12 +164,57 @@ class det_epix100a():
         ds = psana.DataSource(self.fname_xtc)
         det = psana.Detector('XppGon.0:Epix100a.1')
         evt = next(ds.events())
-        s = info_ndarr(det.image(evt), first=10000, last=10005)
+
+        print('ds.env().calibDir:', ds.env().calibDir())
+        print(info_ndarr(det.raw(evt), name='raw', first=10000, last=10005))
+        img = det.image(evt)
+        s = info_ndarr(img, first=10000, last=10005)
         print('1-st event det.image: %s' % s.rstrip('\n'))
+
         assert s == 'shape:(709, 773) size:548057 dtype:float32 [-0.16992188 -0.6699219   2.8300781   1.3300781   0.        ...]'
         self.test_det_completed = True
 
+
+class det_jungfrau():
+
+    def __init__(self):
+        """
+        """
+        print('__init__ for %s' % self.__class__.__name__)
+        self.fname_xtc = path_to_xtc_test_file(fname='data-cxilx7422-r0101-3events-jungfrau4m.xtc')
+        print('fname_xtc: %s' % self.fname_xtc)
+        #print('\n\n======== %s' % self.__class__.__name__) ##.split('_')[-1])
+        #self.dircalib = '/sdf/data/lcls/ds/cxi/cxilx7422/calib'
+        #self.dircalib = './calib'
+        self.dircalib = path_to_calibdir()
+        self.dirwork = './work'
+        self.logname = logname_prefix(self.dirwork, prefix='log-subproc-jungfrau')
+        psana.setOption('psana.calib-dir', self.dircalib)
+        self.test_det_completed = False
+        self.turn_off = False
+
+    def __del__(self):
+        print('__del__ for %s' % self.__class__.__name__)
+
+    def xtc_file_is_available(self):
+        """- test that xtc file is available for jungfrau"""
+        data_exists()
+        path_exists(self.fname_xtc)
+
+    def det_calib(self):
+        """- test command calib.calib for jungfrau"""
+        kwa={'mbits':0}
+        dataset = 'exp=cxilx7422:run=101' # self.fname_xtc
+        #pattern = 'shape:(8, 512, 1024) size:4194304 dtype:float32 [-0.07745222  0.36955786  0.00477444  0.02458291 -0.57804275...]'
+        pattern = 'shape:(8, 512, 1024) size:4194304 dtype:float32 [-0.0747131   0.38151434  0.          0.02539831 -0.585139  ...]'
+        compare_det_calib(dataset, detname='jungfrau4M', expected=pattern, **kwa)
+
+
 epix100a = det_epix100a()
+jungfrau = det_jungfrau()
+
+""" pytest executes all methods with 'test' in the name
+    WORKS FOR COMMAND: pytest Detector/test/pytest_detector.py"""
 
 def test_epix100a_xtc_file_is_available():
     print(sys._getframe().f_code.co_name)
@@ -167,10 +229,23 @@ def test_epix100a_calibrun():
 def test_epix100a_image():
     epix100a.det_image()
 
+def test_jungfrau_xtc_file_is_available():
+    print(sys._getframe().f_code.co_name)
+    jungfrau.xtc_file_is_available()
+
+def test_jungfrau_calib():
+    jungfrau.det_calib()
+
+
 if __name__ == '__main__':
+  """DEBUGGING, WORKS ONLY FOR COMMAND: python Detector/test/pytest_detector.py"""
+  if False:
     test_epix100a_xtc_file_is_available()
     test_epix100a_raw()
     test_epix100a_calibrun()
     test_epix100a_image()
+  if True:
+    test_jungfrau_xtc_file_is_available()
+    test_jungfrau_calib()
 
 # EOF
